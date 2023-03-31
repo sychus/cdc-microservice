d8821f49a0f0a91ac1abb2538ccbd70b069e3e78
Hugo <hugo@blackthorn.io>
2023-03-31 10:31:45 -0300

    add mistery

---
+++ src/mistery.ts
@@ -0,0 +1,23 @@
+function mistery(arr: number[], target: number): number {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (arr[mid] === target) {
+      return mid;
+    } else if (arr[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+}
+
+const arr = [1, 3, 5, 7, 9];
+const target = 5;
+const index = mistery(arr, target);
+
+console.log('Mistery: ', index)
